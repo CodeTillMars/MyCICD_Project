8046371111691ca0cb24d63381bec01c0e71769f
CodeTillMars <catchyourinstructor@email.com>
2020-01-19 21:08:14 +0530

    first programe file

---
+++ addition.java
@@ -0,0 +1,15 @@
+ Volume in drive C has no label.
+ Volume Serial Number is 4224-7089
+
+ Directory of C:\Users\pc\Desktop\My CICD Project
+
+01/19/2020  08:58 PM    <DIR>          .
+01/19/2020  08:58 PM    <DIR>          ..
+01/19/2020  08:58 PM                 0 addition.java
+01/19/2020  08:54 PM               680 hello.class
+01/19/2020  08:54 PM               218 hello.java
+01/19/2020  08:40 PM               220 hello.txt
+01/19/2020  08:54 PM               685 hellotoo.class
+01/19/2020  08:52 PM               220 hellotoo.java
+               6 File(s)          2,023 bytes
+               2 Dir(s)  55,082,291,200 bytes free
---
+++ hellotoo.java
@@ -0,0 +1,14 @@
+public class hellotoo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		for (int i = 1; i <=25; i++) {
+			System.out.println("Hello too world...."+ i);
+		}
+		
+				
+	}
+
+}
